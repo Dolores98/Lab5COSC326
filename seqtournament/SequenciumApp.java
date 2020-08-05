d0cbba054881f1d350fa61204c49df0270293c50
Chris Groenewegen <groch989@studnet.otago.ac.nz>
2020-08-05 16:59:27 +1200

    Removed getName method from SequenciumApp and changed Game variable name

--- seqtournament/SequenciumApp.java
+++ seqtournament/SequenciumApp.java
@@ -6,11 +6,7 @@ import java.util.*;
 public class SequenciumApp implements Player {
 	public static void main(String []args)
 	{
-		Game newGame = new Game(new RandomPlayer(), new SequenciumApp());
-	}
-	public String getName()
-	{
-		return "Team Three Star";
+		Game game = new Game(new RandomPlayer(), new SequenciumApp());
 	}
 	
 }
