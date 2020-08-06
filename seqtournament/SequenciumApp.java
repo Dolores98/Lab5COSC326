5ea6633c2854a48cdc8f07b25568645841b9b6d9
John Kim <kimkw448@student.otago.ac.nz>
2020-08-06 13:04:49 +1200

    chooses the initial hard code move for player one and two.

--- seqtournament/SequenciumApp.java
+++ seqtournament/SequenciumApp.java
@@ -5,8 +5,7 @@ import sequencium.*;
 public class SequenciumApp {
 	public static void main(String []args)
 	{
-		Game game = new Game(new TeamThreeStar(), new RandomPlayer());
-		game.makeVerbose();
+		Game game = new Game(new TeamThreeStar(), new TeamThreeStar());
 		game.reportOutcome();
 		game.run();
 		System.out.println(game.displayBoard());
