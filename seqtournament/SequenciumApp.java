50745c4a45ea4193132a58b53ba464ce7adfc818
John Kim <kimkw448@student.otago.ac.nz>
2020-08-06 12:45:39 +1200

    hardcoded beginnning

--- seqtournament/SequenciumApp.java
+++ seqtournament/SequenciumApp.java
@@ -5,7 +5,7 @@ import sequencium.*;
 public class SequenciumApp {
 	public static void main(String []args)
 	{
-		Game game = new Game(new RandomPlayer(), new TeamThreeStar());
+		Game game = new Game(new TeamThreeStar(), new RandomPlayer());
 		game.makeVerbose();
 		game.reportOutcome();
 		game.run();
--- seqtournament/TeamThreeStar.java
+++ seqtournament/TeamThreeStar.java
@@ -5,76 +5,98 @@ import java.util.*;
 
 public class TeamThreeStar implements Player {
 
-	public String getName()
-	{
+	private boolean initalLoop = true;
+	private int count = 0;
+
+	public String getName() {
 		return "Team Three Star";
 	}
-	
-	public int[] makeMove(int[][] board) 
-	{
-		int[] theMove = new int[3];
-		
-		ArrayList<int[]> possibleMoves = new ArrayList<int[]>();
-		
-		possibleMoves = getPossibleMoves(board);
-		theMove = heuristic(possibleMoves);
-		return theMove;
+
+	public int[] initialMove(int[][] board) {
+		for (int[] is : board) {
+			for (int iss :is){
+				System.out.print(iss);
+			}
+			System.out.println();
+		}
+
+		if (count == 0) {
+			int initialMove[] = { 1, 1, 2 };
+			count++;
+			return initialMove;
+		} else {
+			int initialMove[] = { 2, 2, 3 };
+			initalLoop = false;
+			return initialMove;
+		}
 	}
-	
-	private static ArrayList<int[]> getPossibleMoves(int[][] board) 
-	// rowIndex, colIndex, Highest Possible Value, Highest Enemy Value, AVG friendly value, AVG enemy value, number of enemy tiles, number of friendly tiles
-	// 0,        1,        2,                      3,                   4,                  5,               6,                     7,                       
-	{															   
+
+	public int[] makeMove(int[][] board) {
+		if (initalLoop) {
+			int[] initialMoves = new int[3];
+			initialMoves = initialMove(board);
+			return initialMoves;
+		} else {
+			int[] theMove = new int[3];
+
+			ArrayList<int[]> possibleMoves = new ArrayList<int[]>();
+
+			possibleMoves = getPossibleMoves(board);
+			theMove = heuristic(possibleMoves);
+			return theMove;
+		}
+	}
+
+	private static ArrayList<int[]> getPossibleMoves(int[][] board)
+	// rowIndex, colIndex, Highest Possible Value, Highest Enemy Value, AVG friendly
+	// value, AVG enemy value, number of enemy tiles, number of friendly tiles
+	// 0, 1, 2, 3, 4, 5, 6, 7,
+	{
 		ArrayList<int[]> possibleMoves = new ArrayList<int[]>();
 		ArrayList<int[]> neighbours = new ArrayList<int[]>();
-		
+
 		int boardRows = board.length;
 		int boardCols = board[0].length;
-		
-		for(int i = 0; i < boardRows; i++)
-		{
-			for(int j = 0; j < boardCols; j++)
-			{
-				if(board[i][j] == 0)
-				{
+
+		for (int i = 0; i < boardRows; i++) {
+			for (int j = 0; j < boardCols; j++) {
+				if (board[i][j] == 0) {
 					int highestValue = 0;
 					int highestEnemy = 0;
 					int avgFriendly = 0;
 					int avgEnemy = 0;
 					int friendlyCount = 0;
 					int enemyCount = 0;
-					
+
 					neighbours = Utilities.neighbours(i, j, boardRows, boardCols);
-					for(int k = 0; k < neighbours.size(); k ++)
-					{
+					for (int k = 0; k < neighbours.size(); k++) {
 						int[] coordinate = neighbours.get(k);
-						
+
 						int value = board[coordinate[0]][coordinate[1]];
 
-                                                
-						if(value > 0)
-						{
+						if (value > 0) {
 							friendlyCount += 1;
 							avgFriendly += value;
-							if(value > highestValue) {highestValue = value;}
-						}
-						else if(value < 0)
-						{
+							if (value > highestValue) {
+								highestValue = value;
+							}
+						} else if (value < 0) {
 							enemyCount += 1;
 							avgEnemy += value;
-							if(value < highestEnemy) {highestEnemy = value;}
+							if (value < highestEnemy) {
+								highestEnemy = value;
+							}
 						}
 					}
-					
-					if(highestValue > 0)
-					{
-						
-						avgFriendly = avgFriendly/friendlyCount;
-						if(enemyCount > 0)
-						{
-							avgEnemy = avgEnemy/enemyCount;
+
+					if (highestValue > 0) {
+
+						avgFriendly = avgFriendly / friendlyCount;
+						if (enemyCount > 0) {
+							avgEnemy = avgEnemy / enemyCount;
 						}
-						int[] move = {i, j, highestValue, highestEnemy, avgFriendly, avgEnemy, friendlyCount, enemyCount};
+						int[] move = { i, j, highestValue, highestEnemy, avgFriendly, avgEnemy, friendlyCount,
+								enemyCount };
 						possibleMoves.add(move);
 					}
 				}
@@ -82,49 +104,24 @@ public class TeamThreeStar implements Player {
 		}
 		return possibleMoves;
 	}
-	
-	private static int[] heuristic(ArrayList<int[]> possibleMoves)
-	{
+
+	private static int[] heuristic(ArrayList<int[]> possibleMoves) {
 		int[] bestMove = new int[8];
 		int bestMoveScore = 0;
-		
-		for(int i = 0; i < possibleMoves.size(); i ++)
-		{
+
+		for (int i = 0; i < possibleMoves.size(); i++) {
 			int[] move = possibleMoves.get(i);
 			int moveScore = move[3] + move[6];
-			if(moveScore >= bestMoveScore)
-			{
+			if (moveScore >= bestMoveScore) {
 				bestMove = move;
 				bestMoveScore = moveScore;
 			}
 		}
 		int[] move = new int[3];
-		for(int i = 0; i < 3; i++)
-		{
+		for (int i = 0; i < 3; i++) {
 			move[i] = bestMove[i];
 		}
 		move[2] += 1;
 		return move;
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
