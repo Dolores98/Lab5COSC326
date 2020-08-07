820c20ff8318f038156d1360c4b6dbbfa5749fbc
John Kim <kimkw448@student.otago.ac.nz>
2020-08-07 13:51:54 +1200

    smol error in SequenciumApp

--- seqtournament/SequenciumApp.java
+++ seqtournament/SequenciumApp.java
@@ -5,7 +5,7 @@ import sequencium.*;
 public class SequenciumApp {
 	public static void main(String []args)
 	{
-		Game game = new Game(new TeamThreeStar(), new TeamThreeStar(1));
+		Game game = new Game(new TeamThreeStar(), new TeamThreeStar());
 		game.reportOutcome();
 		game.run();
 		System.out.println(game.displayBoard());
