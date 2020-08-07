539c55c902bdc785e2f1b55597aa99711b7bdf52
Chris Groenewegen <groch989@studnet.otago.ac.nz>
2020-08-07 15:01:11 +1200

    Is now Verbose.

--- seqtournament/SequenciumApp.java
+++ seqtournament/SequenciumApp.java
@@ -6,7 +6,7 @@ public class SequenciumApp {
 	public static void main(String[] args) {
 
 		for (int i = 0; i < 10; i++) {
-			Game game = new Game(new TeamThreeStar(3), new TeamThreeStar());
+			Game game = new Game(new TeamThreeStar(3), new RandomPlayer());
 			game.run();
 			// System.out.println(game.displayBoard());
 		}
