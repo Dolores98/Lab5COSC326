0790e01c5f9f8bfbb6a494b93676a948a6663a6f
John Kim <kimkw448@student.otago.ac.nz>
2020-08-07 14:50:57 +1200

    cleaned up the main.

--- seqtournament/SequenciumApp.java
+++ seqtournament/SequenciumApp.java
@@ -5,13 +5,10 @@ import sequencium.*;
 public class SequenciumApp {
 	public static void main(String[] args) {
 
-		// System.out.println(game.displayBoard());
-
 		for (int i = 0; i < 10; i++) {
 			Game game = new Game(new TeamThreeStar(3), new TeamThreeStar());
 			game.run();
-			//game.reportOutcome();
-			//System.out.println(game.getLog());
+			// System.out.println(game.displayBoard());
 		}
 	}
 
