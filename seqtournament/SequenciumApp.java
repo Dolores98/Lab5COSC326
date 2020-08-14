1413ca2748810668d4f9f88149e63bfde8d7d226
John Kim <kimkw448@student.otago.ac.nz>
2020-08-14 17:03:26 +1200

    There was an in built print statement....

--- seqtournament/SequenciumApp.java
+++ seqtournament/SequenciumApp.java
@@ -6,14 +6,14 @@ import sequencium.RandomPlayer;
 public class SequenciumApp {
 	public static void main(String[] args) {
 
-		for (int i = 0; i < 20; i++) {
+		for (int i = 0; i < 1; i++) {
 			// Game game = new Game(new TeamThreeStar(3), new TeamThreeStar(2));
 			// Game game = new Game(new TeamThreeStar(2), new TeamThreeStar(3));
-			Game game = new Game(new TeamThreeStar(), new RandomPlayer());
+			Game game = new Game(new TeamThreeStar(), new RandomPlayer(), 10, 10);
 
-
+			// game.makeVerbose();
 			game.run();
-			// System.out.println(game.displayBoard());
+			// System.out.println(game.getLog());
 		}
 	}
 
